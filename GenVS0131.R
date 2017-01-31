ef12586762440a317eef2565c3233a018b30ef1b
Hans Lehndorff <lehndorff@evergreenecon.com>
2017-01-31 12:22:51 -0800

    Toggle

--- GenVS0131.R
+++ GenVS0131.R
@@ -39,8 +39,8 @@ subsetsx<-function(data=Dataopt, size=StratVar, strata="Work"){
 
 # Optimization inputs; # of Strata, which End Uses, Sum kWh variation tolerance, intial Critial Value and Percision
 Strata<-4
-Endusesn<-c(1:3,5)
-ToleranceSet<-1.3
+Endusesn<-c(1,3,4,6,7,9)
+ToleranceSet<-1.5
 Critical<-1.645
 Precision<-.1
 ###
