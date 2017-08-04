845f7570c6034aece926f105d2d9afd16b55510a
Hans Lehndorff <lehndorff@evergreenecon.com>
2017-08-04 14:17:48 -0700

    toggle

--- RFbit2.R
+++ RFbit2.R
@@ -13,8 +13,9 @@ ptm<-proc.time()
 Marks<-c(65,124,189,253,337,420,505,1008,1511,3000)
 Historyday<-NULL
 Historyrun<-NULL
+RunOn<-FALSE
 reset<-1
-for (z in 5:5){
+for (z in 120:2){
   bit<-z
   PctupRun<-NULL
   PctupDay<-NULL
@@ -44,92 +45,100 @@ for (h in Marks){
   Run<-NULL
   for (j in 1:length(symbols)){
     Last<-h
-    STOCK<-stock()
-    STOCKstreak<-STOCK%>%group_by(streak)%>%summarise(strkrf=mean(rflag,na.rm=TRUE),strkup=sum(lag,na.rm=TRUE),strkn=n(),strkpct=strkup/strkn)
+    STOCK<-stock(RUN = RunOn)
+    if(RunOn==TRUE){STOCKstreak<-STOCK%>%group_by(streak)%>%summarise(strkrf=mean(rflag,na.rm=TRUE),strkup=sum(lag,na.rm=TRUE),strkn=n(),strkpct=strkup/strkn)}
     qSTOCK<-q[q$V1%in%symbols[j],]
     quote<-qSTOCK$Change/qSTOCK$Last*100
-    Run<-c(Run,sum(as.numeric(STOCKstreak$streak[runtd()])))
+    if(RunOn==TRUE){
+      Run<-c(Run,sum(as.numeric(STOCKstreak$streak[runtd()])))
+      Runpct<-c(Runpct,sum(as.numeric(STOCKstreak$strkpct[runtd()])))
+      Runrf<-c(Runrf,sum(as.numeric(STOCKstreak$strkrf[runtd()])))
+      RunDen<-c(RunDen,sum(as.numeric(STOCKstreak$strkn[runtd()])))
+    }
     Day<-c(Day,round(quote,3))
-    Runpct<-c(Runpct,sum(as.numeric(STOCKstreak$strkpct[runtd()])))
     Daypct<-c(Daypct,mean(STOCK$lag[between(STOCK$pCHANGE,drange()[1],drange()[2])],na.rm = TRUE))
-    Runrf<-c(Runrf,sum(as.numeric(STOCKstreak$strkrf[runtd()])))
     Dayrf<-c(Dayrf,mean(STOCK$rflag[between(STOCK$pCHANGE,drange()[1],drange()[2])],na.rm = TRUE))
-    RunDen<-c(RunDen,sum(as.numeric(STOCKstreak$strkn[runtd()])))
     DayDen<-c(DayDen,sum(!is.na(STOCK$lag[between(STOCK$pCHANGE,drange()[1],drange()[2])])))
   }
-  PctupRun<-cbind(PctupRun,Runpct)
+  if(RunOn==TRUE){
+    PctupRun<-cbind(PctupRun,Runpct)
+    PctrfRun<-cbind(PctrfRun,Runrf)
+    DenRun<-cbind(DenRun,RunDen)
+  }
   PctupDay<-cbind(PctupDay,Daypct)
-  PctrfRun<-cbind(PctrfRun,Runrf)
   PctrfDay<-cbind(PctrfDay,Dayrf)
-  DenRun<-cbind(DenRun,RunDen)
   DenDay<-cbind(DenDay,DayDen)
-  print(h)
+  # print(h)
 }
 cols<-paste0(Marks,sep="x")
-PctupRun<-as.data.frame(cbind(symbols,Run,PctupRun))
-names(PctupRun)<-c("symb","Run",cols)
-PctupRun[,cols]<-sapply(PctupRun[,cols],as.character)
-PctupRun[,cols]<-sapply(PctupRun[,cols],as.numeric)
+if(RunOn==TRUE){
+  PctupRun<-as.data.frame(cbind(symbols,Run,PctupRun))
+  names(PctupRun)<-c("symb","Run",cols)
+  PctupRun[,cols]<-sapply(PctupRun[,cols],as.character)
+  PctupRun[,cols]<-sapply(PctupRun[,cols],as.numeric)
+  PctrfRun<-as.data.frame(cbind(symbols,Run,PctrfRun))
+  names(PctrfRun)<-c("symb","Run",cols)
+  PctrfRun[,cols]<-sapply(PctrfRun[,cols],as.character)
+  PctrfRun[,cols]<-sapply(PctrfRun[,cols],as.numeric)
+  DenRun<-as.data.frame(cbind(symbols,Run,DenRun))
+  names(DenRun)<-c("symb","Run",cols)
+  DenRun[,cols]<-sapply(DenRun[,cols],as.character)
+  DenRun[,cols]<-sapply(DenRun[,cols],as.numeric)
+}
 PctupDay<-as.data.frame(cbind(symbols,Day,PctupDay))
 names(PctupDay)<-c("symb","Day",cols)
 PctupDay[,cols]<-sapply(PctupDay[,cols],as.character)
 PctupDay[,cols]<-sapply(PctupDay[,cols],as.numeric)
-PctrfRun<-as.data.frame(cbind(symbols,Run,PctrfRun))
-names(PctrfRun)<-c("symb","Run",cols)
-PctrfRun[,cols]<-sapply(PctrfRun[,cols],as.character)
-PctrfRun[,cols]<-sapply(PctrfRun[,cols],as.numeric)
 PctrfDay<-as.data.frame(cbind(symbols,Day,PctrfDay))
 names(PctrfDay)<-c("symb","Day",cols)
 PctrfDay[,cols]<-sapply(PctrfDay[,cols],as.character)
 PctrfDay[,cols]<-sapply(PctrfDay[,cols],as.numeric)
-DenRun<-as.data.frame(cbind(symbols,Run,DenRun))
-names(DenRun)<-c("symb","Run",cols)
-DenRun[,cols]<-sapply(DenRun[,cols],as.character)
-DenRun[,cols]<-sapply(DenRun[,cols],as.numeric)
 DenDay<-as.data.frame(cbind(symbols,Day,DenDay))
 names(DenDay)<-c("symb","Day",cols)
 DenDay[,cols]<-sapply(DenDay[,cols],as.character)
 DenDay[,cols]<-sapply(DenDay[,cols],as.numeric)
 
-PctupRun$min<-0
+if(RunOn==TRUE){
+  PctupRun$min<-0
+  PctupRun$max<-0
+  PctupRun$mean<-0
+}
 PctupDay$min<-0
-PctupRun$max<-0
 PctupDay$max<-0
-PctupRun$mean<-0
 PctupDay$mean<-0
 for (j in 1:length(symbols)){
-  PctupRun$min[j]<-min(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
+  if(RunOn==TRUE){
+    PctupRun$min[j]<-min(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
+    PctupRun$max[j]<-max(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
+    PctupRun$mean[j]<-mean(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
+  }
   PctupDay$min[j]<-min(as.numeric(PctupDay[j,cols]),na.rm=TRUE)
-  PctupRun$max[j]<-max(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
   PctupDay$max[j]<-max(as.numeric(PctupDay[j,cols]),na.rm=TRUE)
-  PctupRun$mean[j]<-mean(as.numeric(PctupRun[j,cols]),na.rm=TRUE)
   PctupDay$mean[j]<-mean(as.numeric(PctupDay[j,cols]),na.rm=TRUE)
 }
-PctupRun$min[is.infinite(PctupRun$min)]<-0
+if(RunOn==TRUE){
+  PctupRun$min[is.infinite(PctupRun$min)]<-0
+  PctupRun$max[is.infinite(PctupRun$max)]<-0
+  PctupRun$mean[is.nan(PctupRun$mean)]<-0
+}
 PctupDay$min[is.infinite(PctupDay$min)]<-0
-PctupRun$max[is.infinite(PctupRun$max)]<-0
 PctupDay$max[is.infinite(PctupDay$max)]<-0
-PctupRun$mean[is.nan(PctupRun$mean)]<-0
 PctupDay$mean[is.nan(PctupDay$mean)]<-0
 
-checkUR<-as.vector(PctupRun$symb[PctupRun$min>.5&PctupRun$max>=.6&(PctupRun$min==PctupRun$`3000x`|PctupRun$max==PctupRun$`65x`)])
+if(RunOn==TRUE){
+  checkUR<-as.vector(PctupRun$symb[PctupRun$min>.5&PctupRun$max>=.6&(PctupRun$min==PctupRun$`3000x`|PctupRun$max==PctupRun$`65x`)])
+  checkDR<-as.vector(DenRun$symb[DenRun$`65x`>=8&DenRun$`3000x`>=50])
+  checkU<-checkUR[checkUR%in%checkUD]
+  symbolsWatchR<-checkUR[checkUR%in%checkDR]
+  WatchRun<-PctupRun[PctupRun$symb%in%symbolsWatchR,]
+}
 checkUD<-as.vector(PctupDay$symb[PctupDay$min>.5&PctupDay$max>=.6&(PctupDay$min==PctupDay$`3000x`|PctupDay$max==PctupDay$`65x`)])
-# checkRR<-as.vector()
-# checkRD<-as.vector()
-checkDR<-as.vector(DenRun$symb[DenRun$`65x`>=8&DenRun$`3000x`>=50])
 checkDD<-as.vector(DenDay$symb[DenDay$`65x`>=8&DenDay$`3000x`>=50])
-checkU<-checkUR[checkUR%in%checkUD]
 checkD<-checkDD[checkDD%in%checkDR]  
 symbolsWatchRD<-checkU[checkU%in%checkD]
 symbolsWatchD<-checkUD[checkUD%in%checkDD]
-symbolsWatchR<-checkUR[checkUR%in%checkDR]
-
 WatchDay<-PctupDay[PctupDay$symb%in%symbolsWatchD,]
-WatchRun<-PctupRun[PctupRun$symb%in%symbolsWatchR,]
-# WatchDay$both<-0
-# WatchRun$both<-0
-# WatchDay$both[WatchDay$symb%in%symbolsWatchRD]<-1
-# WatchRun$both[WatchRun$symb%in%symbolsWatchRD]<-1
+
   
   Tomorrow<-as.data.frame(matrix(data = NA, nrow=length(symbols),ncol = 5))
   names(Tomorrow)<-c("V1","Last","Change","Ctype","sign")
@@ -142,12 +151,19 @@ WatchRun<-PctupRun[PctupRun$symb%in%symbolsWatchR,]
   }
   rownames(Tomorrow)<-Tomorrow$V1
   Tomorrow$sign<-sign(Tomorrow$Change)
+  if(RunOn==TRUE){
+    TomorrowWatchR<-Tomorrow[Tomorrow$V1 %in% symbolsWatchR,]
+    TomorrowWatchR<-TomorrowWatchR[order(TomorrowWatchR$V1),]
+    Printrun<-cbind(merge(select(WatchRun,symb,Run,min,max,mean),select(TomorrowWatchR,V1,Last,Change,sign),by.x="symb",by.y = "V1"), date=row.names(as.data.frame(STOCK))[nrow(STOCK)-bit+1])
+    Historyrun<-rbind(Historyrun,Printrun)
+  }
   TomorrowWatchD<-Tomorrow[Tomorrow$V1 %in% symbolsWatchD,]
-  TomorrowWatchR<-Tomorrow[Tomorrow$V1 %in% symbolsWatchR,]
   TomorrowWatchD<-TomorrowWatchD[order(TomorrowWatchD$V1),]
-  TomorrowWatchR<-TomorrowWatchR[order(TomorrowWatchR$V1),]
+  Printday<-cbind(merge(select(WatchDay,symb,Day,min,max,mean),select(TomorrowWatchD,V1,Last,Change,sign),by.x="symb",by.y = "V1"), date=row.names(as.data.frame(STOCK))[nrow(STOCK)-bit+1])
+  Historyday<-rbind(Historyday,Printday)
   
   print(row.names(as.data.frame(STOCK))[nrow(STOCK)-bit+1])
+  print(sum(Printday$sign[Printday$sign==1])/length(Printday$sign))
   # print("Day")
   # print(sum(sign(as.numeric(as.vector(TomorrowWatchD$sign)))>0)/length(TomorrowWatchD$sign))
   # Dayup<-(Dayup+sum(sign(as.numeric(as.vector(TomorrowWatchD$sign)))>0))*reset
@@ -174,11 +190,6 @@ WatchRun<-PctupRun[PctupRun$symb%in%symbolsWatchR,]
   # print(sum(TomorrowWatchR$Change))
   # print(sum(sign(PctupDay$mean-.5)==sign(Tomorrow$Change))>=sum(sign(PctupRun$mean-.5)==sign(Tomorrow$Change)))
 
-
-  Printday<-cbind(merge(select(WatchDay,symb,Day,min,max,mean),select(TomorrowWatchD,V1,Last,Change,sign),by.x="symb",by.y = "V1"), date=row.names(as.data.frame(STOCK))[nrow(STOCK)-bit+1])
-  Historyday<-rbind(Historyday,Printday)
-  Printrun<-cbind(merge(select(WatchRun,symb,Run,min,max,mean),select(TomorrowWatchR,V1,Last,Change,sign),by.x="symb",by.y = "V1"), date=row.names(as.data.frame(STOCK))[nrow(STOCK)-bit+1])
-  Historyrun<-rbind(Historyrun,Printrun)
   # print((proc.time()-ptm)/60)
 }
 (proc.time()-ptm)/60
