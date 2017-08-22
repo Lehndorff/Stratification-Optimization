8c7b92c87902988f2575ac1d2a56be962a345249
Hans Lehndorff <lehndorff@evergreenecon.com>
2017-08-22 09:00:34 -0700

    toggle

--- RFfunct.R
+++ RFfunct.R
@@ -4,31 +4,35 @@ drange<-function(bound=.2,bump=.075){
 }
 stock<-function(STOCK=get(symbols[j]),Last=h,RUN=TRUE){
   colnames(STOCK)<-c("open","high","low","close","volume","adjusted")
-    if(length(STOCK$open)<Last){Last<-length(STOCK$open)}
-    STOCK<-STOCK[(length(STOCK$open)-Last):length(STOCK$open),]
-    STOCK$row<-1:length(STOCK$open)
-    STOCK$pCHANGE<-((STOCK$close)-lag(STOCK$close))/lag(STOCK$close)*100
-    STOCK$UP<-0
-    STOCK$UP[sign(STOCK$pCHANGE)==1]<-1
-    if (RUN == TRUE){
-      streak<-data.frame(unclass(rle(as.vector(STOCK$UP))))
-      streak$values[streak$values==0]<--1
-      streak$tvalue<-streak$lengths*streak$values
-      y<-as.vector(NULL)
-      for(i in 1:length(streak$lengths)){
-        y<-c(y,(streak$values[i]:streak$tvalue[i]))
-      }
-      STOCK<-cbind(STOCK,y)
+  if(length(STOCK$open)<Last){Last<-length(STOCK$open)}
+  STOCK<-STOCK[(length(STOCK$open)-Last):length(STOCK$open),]
+  STOCK$row<-1:length(STOCK$open)
+  STOCK$pCHANGE<-((STOCK$close)-lag(STOCK$close))/lag(STOCK$close)*100
+  STOCK$UP<-0
+  STOCK$UP[sign(STOCK$pCHANGE)==1]<-1
+  if (RUN == TRUE){
+    streak<-data.frame(unclass(rle(as.vector(STOCK$UP))))
+    streak$values[streak$values==0]<--1
+    streak$tvalue<-streak$lengths*streak$values
+    y<-as.vector(NULL)
+    for(i in 1:length(streak$lengths)){
+      y<-c(y,(streak$values[i]:streak$tvalue[i]))
     }
-    STOCK<-as.data.frame(STOCK)
-    STOCK$lag<-lead(STOCK$UP,1)
-    STOCK$rflag<-lead(STOCK$pCHANGE)
-    colnames(STOCK)[colnames(STOCK)=="..2"]<-"streak"
-    return(STOCK)
+    STOCK<-cbind(STOCK,y)
+  }
+  STOCK<-as.data.frame(STOCK)
+  STOCK$lag<-lead(STOCK$UP,1)
+  STOCK$rflag<-lead(STOCK$pCHANGE)
+  colnames(STOCK)[colnames(STOCK)=="..2"]<-"streak"
+  return(STOCK)
 }
 
 runtd<-function(){
   STOCKstreak$streak==(qSTOCK$sign+(STOCK$streak[length(STOCK$streak)]*as.numeric(sign(STOCK$streak[length(STOCK$streak)])==qSTOCK$sign)))
+}
+
+runtd2<-function(){
+  (qSTOCK$sign+(STOCK$streak[length(STOCK$streak)]*as.numeric(sign(STOCK$streak[length(STOCK$streak)])==qSTOCK$sign)))
 }
 
 STREAK<-function(){
@@ -45,4 +49,4 @@ for (j in 1:length(symbols2)){
   symbols2<-symbols2[!(symbols2 %in% symbols2[j])]
   }
 }
-
+Historyday<-Historyday[Historyday$symb %in% symbols2,]
